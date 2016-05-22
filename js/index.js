a18af4065c5e3b2f1edd2b30ee91283e3e43100e
luizespindola <luizhenrique_espindola@hotmail.com>
2016-05-22 18:27:13 -0300

    Quatro primeiras apresentações prontas.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,45 @@
 window.onload = function(){
-	header = document.getElementsByClassName("page-header-git");
+	header = document.getElementById("project-name");
+
+	header.onclick = function() {
+		window.location = "http://luizespindola.github.io/index.html";
+//		window.location = "file:///C:/Users/Luiz/workspace/introducaoinformatica/index.html";
+	};
+	
+	content = document.getElementById("content");
+	theme1 = document.getElementById("theme1");
+	theme2 = document.getElementById("theme2");
+	theme3 = document.getElementById("theme3");
+	
+	if(theme1 !== null){
+		theme1.onclick = function(){
+			$(content.children[0]).css('display','block');
+			$(content.children[1]).css('display','none');
+			$(content.children[2]).css('display','none');
+		};
+	}
+	
+	if(theme2 !== null){
+		theme2.onclick = function(){
+			$(content.children[0]).css('display','none');
+			$(content.children[1]).css('display','block');
+			$(content.children[2]).css('display','none');
+		};
+	}
+	
+	if(theme3 !== null){
+		theme3.onclick = function(){
+			$(content.children[0]).css('display','none');
+			$(content.children[1]).css('display','none');
+			$(content.children[2]).css('display','block');
+		};
+	}
 }
 
 var header;
+var content;
+var theme1;
+var theme2;
+var theme3;
+
+
